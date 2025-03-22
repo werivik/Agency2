59af7094e460bdd80c02513c7ba00c31dd365290
werivik <wervik00994@stud.noroff.no>
2025-03-22 12:56:19 +0100

    made a header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,19 @@
 import styles from './Header.module.css';
 import { Link } from "react-router-dom";
+import headerLogo from '/media/logo/Logo.png';
 
 function Header() {
   return (
     <header>
+      <nav>
+        <Link to="/" className={styles.logo}><img src={headerLogo} alt="The Spot Logo" className={styles.headerLogo}></img></Link>
+        <ul>
+          <Link to="/" className={styles.logo}>Home</Link>
+          <Link to="/" className={styles.logo}>Login</Link>
+          <Link to="/" className={styles.logo}>About us</Link>
+          <Link to="/" className={styles.logo}>Contact</Link>
+        </ul>
+      </nav>
     </header>
   );
 }
