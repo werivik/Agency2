5cf09e727e0d22ade8a3d6b91085a559103ceaf4
werivik <wervik00994@stud.noroff.no>
2025-03-24 14:29:41 +0100

    basic contact page styling

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -13,9 +13,18 @@ function Contact() {
         <div className={styles.contactContent}>
           <p>If you are having any troubles or questions, do not hesitate to contact us. You can find our contact info below:</p>
           <div className={styles.ContactInfo}>
-            <p>support@thespot.com</p>
-            <p>ABC Road, Bergen</p>
-            <p>(+47) 123 45 678</p>
+            <div>
+              <p>Email:</p>
+              <p>support@thespot.com</p>
+            </div>
+            <div>
+              <p>Location:</p>
+              <p>ABC Road, Bergen</p>
+            </div>
+            <div>
+              <p>Tlf:</p>
+              <p>(+47) 123 45 678</p>
+            </div>
           </div>
         </div>
       </div>
