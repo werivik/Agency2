774289b4b0a8c28469daedf86d464608eaa91b2f
werivik <wervik00994@stud.noroff.no>
2025-03-24 13:33:34 +0100

    trying still to fix the img bug

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
+
 import Home from './pages/Home/Home';
 import Login from './pages/Login/Login';
 import Register from './pages/Register/Register';
