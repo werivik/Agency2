af2591e792288b2ead3ea06e002c3c96e6f4a90b
Ole <144427985+Ole12345678910@users.noreply.github.com>
2025-03-25 16:21:16 +0100

    updated the main into the current branch

--- src/pages/Events/Events.jsx
+++ src/pages/Events/Events.jsx
@@ -8,43 +8,57 @@ import { db } from "../../firebase";
 import { collection, getDocs } from "firebase/firestore";
 
 const Home = () => {
-  const [events, setEvents] = useState([]);
-  const [filteredEvents, setFilteredEvents] = useState([]);
+    const [events, setEvents] = useState([]);
+    const [filteredEvents, setFilteredEvents] = useState([]);
 
-  useEffect(() => {
-    const fetchEvents = async () => {
-      try {
-        const querySnapshot = await getDocs(collection(db, "events"));
-        const eventsList = querySnapshot.docs.map((doc) => ({
-          id: doc.id,
-          ...doc.data(),
-        }));
-        setEvents(eventsList);
-        setFilteredEvents(eventsList);
-      } catch (error) {
-        console.error("Error fetching events:", error);
-      }
+    useEffect(() => {
+        const fetchEvents = async () => {
+            try {
+                const querySnapshot = await getDocs(collection(db, "events"));
+                const eventsList = querySnapshot.docs.map(doc => ({
+                    id: doc.id,
+                    ...doc.data(),
+                }));
+                setEvents(eventsList);
+                setFilteredEvents(eventsList.slice(0, 6));
+            } 
+            
+            catch (error) {
+                console.error("Error fetching events:", error);
+            }
+        };
+
+        fetchEvents();
+    }, []);
+
+    const formatDate = (timestamp) => {
+        const date = timestamp?.toDate();
+        if (!date) return "No date available";
+    
+        const formattedDate = date.toLocaleDateString('en-US', { month: 'long', day: 'numeric' });
+    
+        const monthAbbreviations = {
+            January: "Jan",
+            February: "Feb",
+            August: "Aug",
+            September: "Sept",
+            November: "Nov",
+            December: "Dec"
+        };
+    
+        return formattedDate.replace(
+            /\b(January|February|August|September|November|December)\b/,
+            (match) => monthAbbreviations[match]
+        );
     };
-
-    fetchEvents();
-  }, []);
-
-  const formatDate = (timestamp) => {
-    const date = timestamp?.toDate();
-    return date
-      ? date.toLocaleDateString("en-US", { month: "long", day: "numeric" })
-      : "No date available";
-  };
-
   return (
     <div>
       <section>
         <div>
           <section className={styles.mainText}>
             <h1>Explore Events</h1>
-            <button>Filters</button>
+            <button className={styles.filterButton}>Filters</button>
           </section>
-
           <div className={styles.eventContainer}>
             {filteredEvents.length > 0 ? (
               filteredEvents.map((event) => (
