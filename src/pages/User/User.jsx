097021fdaf4c490f7c71d5464b991127fd2b0862
werivik <wervik00994@stud.noroff.no>
2025-03-26 17:06:49 +0100

    User page Content + Styling

--- src/pages/User/User.jsx
+++ src/pages/User/User.jsx
@@ -1,11 +1,35 @@
-/* This is the Users Page, the logged in user can view their account using this page*/
-
 import styles from './User.module.css';
 import { Link } from "react-router-dom";
+import defaultProfile from "/media/gallery/defaultprofile.png";
 
 function User() {
   return (
-    <div>
+    <div className={styles.userBackground}>
+      <section className={styles.profileSection}>
+        <div className={styles.profileBorder}>
+          <div className={styles.profileInfo}>
+            <div className={styles.profileLeft}>
+              <div className={styles.profileImage}>
+                <img src={defaultProfile}></img>
+              </div>
+              <div className={styles.profileName}>
+                <p>Username</p>
+              </div>
+            </div>
+            <div className={styles.profileRight}>
+              <p>Events Saved</p>
+              <p>Favorite Organisations</p>
+            </div>
+          </div>
+
+          <div className={styles.profileOptions}>
+            <p>Create Event</p>
+            <p>Edit Event</p>
+            <p>Profile Settings</p>
+            <p>Sign Out</p>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
