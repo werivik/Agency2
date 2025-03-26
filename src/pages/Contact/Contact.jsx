44a44479ac10da0c7ae8a99265558b2a43b459dd
werivik <wervik00994@stud.noroff.no>
2025-03-26 15:36:48 +0100

    styled Contact page

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -3,29 +3,35 @@
 import styles from './Contact.module.css';
 import { Link } from 'react-router-dom';
 import { useEffect, useState } from 'react';
+import bannerImage from '/media/gallery/bergensentrum.jpg';
 
 function Contact() {
 
   return (
     <section className={styles.contactSection}>
       <div className={styles.contactBorder}>
+        <div className={styles.contactLeft}>
+          <img src={bannerImage} ></img>
+        </div>
+        <div  className={styles.contactRight}>
         <h1>Contact Us</h1>
         <div className={styles.contactContent}>
           <p>If you are having any troubles or questions, do not hesitate to contact us. You can find our contact info below:</p>
           <div className={styles.ContactInfo}>
             <div>
-              <p>Email:</p>
+              <span>Email:</span>
               <p>support@thespot.com</p>
             </div>
             <div>
-              <p>Location:</p>
+              <span>Location:</span>
               <p>ABC Road, Bergen</p>
             </div>
             <div>
-              <p>Tlf:</p>
+              <span>Tlf:</span>
               <p>(+47) 123 45 678</p>
             </div>
           </div>
+        </div>
         </div>
       </div>
     </section>
