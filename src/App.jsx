eab5fe572eabc0848893a9aeb9e4374206ef2d19
werivik <wervik00994@stud.noroff.no>
2025-03-27 14:27:09 +0100

    Update App.jsx
    Trying to fix Netlify issue

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
-
 import Home from './pages/Home/Home';
 import LoginOptions from './pages/LoginOptions/LoginOptions';
 import Login from './pages/Login/Login';
